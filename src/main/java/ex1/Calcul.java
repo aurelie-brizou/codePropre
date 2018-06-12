841954ce8447d1588382aba00caaeee95c52f326
Aurelie Brizou <brizouaurelie@gmail.com>
2018-06-12 14:23:01 +0200

    correction selon la règle de nommage de l'exo1

---
+++ src/main/java/ex1/Calcul.java
@@ -0,0 +1,23 @@
+package ex1;
+
+
+public class Calcul {
+
+	/**
+	 * 2 entiers en arguments
+	 * Additionner  
+	 * @author Aurelie-B
+	 */
+	public int addition (int a, int b) {
+		return a+b;
+	}
+	
+	/**
+	 * 2 entiers en arguments
+	 * Soustraire 
+	 * @author Aurelie-B
+	 */
+	public int soustraction (int a, int b) {
+		return a-b;
+	}
+}
---
+++ src/main/java/ex1/Entreprise.java
@@ -0,0 +1,23 @@
+package ex1;
+
+import java.util.Date;
+
+
+public class Entreprise {
+
+	public int siret;
+	public String nom;
+	public String adresse;
+	public Date dateCreation;
+	
+	public static final int CAPITAL_MAX = 3000000;
+	
+	/**
+	 * Afficher le statut d'une entreprise
+	 * @author Aurelie-B
+	 */
+	public void afficherStatut(){
+		
+	}
+	
+}
