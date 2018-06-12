8eeeca8b4cbc11b63dd97663dfbf344728d9c8e2
Aurelie Brizou <brizouaurelie@gmail.com>
2018-06-12 15:18:06 +0200

    Correction exo 2

--- src/main/java/ex2/LivretA.java
+++ src/main/java/ex2/LivretA.java
@@ -2,8 +2,30 @@ package ex2;
 
 public class LivretA extends CompteBancaire {
 
-	public LivretA(String type, double solde, double tauxRemuneration) {
-		super(type, solde, 0, tauxRemuneration);
+	/** tauxRemuneration d'un livret A */
+	private double tauxRemuneration;
+
+	public LivretA(double solde, double tauxRemuneration) {
+		super(solde, 0);
+		this.tauxRemuneration = tauxRemuneration;
+	}
+
+	public void appliquerRemuAnnuelle(){
+			this.solde = solde + solde*tauxRemuneration/100;
+	}
+	
+	/** Getter for tauxRemuneration
+	 * @return the tauxRemuneration
+	 */
+	public double getTauxRemuneration() {
+		return tauxRemuneration;
+	}
+	
+	/** Setter
+	 * @param tauxRemuneration the tauxRemuneration to set
+	 */
+	public void setTauxRemuneration(double tauxRemuneration) {
+		this.tauxRemuneration = tauxRemuneration;
 	}
 
 }
